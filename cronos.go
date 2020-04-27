24c320d8d0600cc4ce50242104d87179c30a6bb1
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2020-04-27 10:07:02 -0300

    Implementing Fetch function in Cronos

--- cronos.go
+++ cronos.go
@@ -10,8 +10,48 @@ type Cronos struct {
 	cache
 }
 
+func (cronos *Cronos) invokeConstructor(constructor constructor,
+	args []reflect.Type) (returns []reflect.Value) {
+
+	dependencies := make([]reflect.Value, len(args))
+
+	for i := 0; i < len(dependencies); i++ {
+		dependencie := cronos.Fetch(args[i])
+		dependencies[i] = reflect.ValueOf(dependencie)
+	}
+
+	constructorValue := reflect.ValueOf(constructor)
+
+	returns = constructorValue.Call(dependencies)
+	return
+}
+
 func (cronos *Cronos) Fetch(typed reflect.Type) interface{} {
-	return nil
+	key := key{typed}
+
+	if object, found := cronos.cache.components[key]; found {
+		return object
+	}
+
+	constructor := cronos.cache.constructors[key]
+	args := cronos.getArgs(constructor)
+
+	returns := cronos.invokeConstructor(constructor, args)
+
+	if len(returns) == 2 && !returns[1].IsNil() {
+		err := returns[1].Interface().(error)
+		panic(err)
+	}
+
+	object := returns[0].Interface()
+
+	isNotSingleton := cronos.cache.notSingletons[key]
+
+	if !isNotSingleton {
+		cronos.cache.components[key] = object
+	}
+
+	return object
 }
 
 func (cronos *Cronos) getArgs(constructor constructor) []reflect.Type {
